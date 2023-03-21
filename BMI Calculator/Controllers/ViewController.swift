78b812da74d93628393175a713c1b6516db7b1f5
Anisa Zahra <anisafatimahazzahra@gmail.com>
2023-03-21 17:25:23 +0700

    adding calculation for BMI

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -9,12 +9,45 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var hSliderValue = 1.0
+    var wSliderValue = 60
+    var bmiValue = 0.0
 
+    @IBOutlet weak var weightValue: UILabel!
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var heightValue: UILabel!
+    @IBOutlet weak var weightSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setSliderValue()
+        weightSlider.value = Float(wSliderValue)
+        heightSlider.value = Float(hSliderValue)
     }
-
-
+    
+    @IBAction func heightSlider(_ sender: UISlider) {
+        hSliderValue = Double(String(format: "%.2f", sender.value)) ?? 0
+        setSliderValue()
+    }
+    
+    @IBAction func weightSlider(_ sender: UISlider) {
+        wSliderValue = Int(sender.value)
+        setSliderValue()
+    }
+    
+    func setSliderValue() {
+        heightValue.text = "\(hSliderValue)" + "m"
+        weightValue.text = "\(wSliderValue)" + "Kg"
+    }
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        calculateBMI()
+    }
+    
+    func calculateBMI(){
+        bmiValue = Double(wSliderValue) / (hSliderValue*hSliderValue)
+        
+        print(bmiValue)
+    }
 }
 
